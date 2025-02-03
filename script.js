0721ed78f02f04633547959c02d5e2edbe7b9e41
Carlos Antonio <carlosabcj1@gmail.com>
2025-02-03 16:19:21 -0300

    carrossel

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+ * Ano automático
+ * @author Carlos Antonio
+ */
+
+// banner carrossel 
+const main = document.querySelector('main')
+const backgrounds = ['url(img/ft1.jpg)','url(img/ft2.jpg)','url(img/ft3.jpg)']
+
+let indice = 0
+
+function slider(){
+    indice ++
+    if (indice >= backgrounds.length) {
+        indice = 0
+    }
+    // troca da imagem de background no CSS
+main.style.backgroundImage = backgrounds[indice]
+main.style.transtion = 'background-image 1s ease-in-out'
+}
+
+setInterval(slider, 2000)
+// inserindo o ano na tag <span>
+let ano = document.getElementById('copyrightYear')
+// a linha abaixo obtem o ano atuali do sistema
+let anoAtual = new Date().getFullYear()
+// a linha abaixo atribui o ano a tag <span>
+ano.innerHTML = anoAtual
